b8550c336c15ad3a4815ce07f0f19277e2ec7f40
Levon Rush <l.rush@nib.com.au>
2023-03-08 20:18:51 +1100

    updates to get working again..

--- R/clean-data.R
+++ R/clean-data.R
@@ -13,17 +13,17 @@ clean_data <- function(data){
     mutate(team_home = fct_expand(team_home, "Dolphins"),
            team_away = fct_expand(team_away, "Dolphins"))
   
-  # R1 ladder positions move to the end of last year - this is a bad soln and needs updating
-  data <- data %>%
-    arrange(game_id) %>%
-    group_by(team_home) %>%
-    mutate(team_position_home = ifelse(round_id == 1, lag(team_position_home), team_position_home)) %>%
-    group_by(team_away) %>%
-    mutate(team_position_away = ifelse(round_id == 1, lag(team_position_away), team_position_away)) %>%
-    ungroup() %>%
-    # fix intro for dolphins
-    mutate(team_position_home = ifelse(game_id == 20231110170, 17, team_position_home))
-    
+  # # R1 ladder positions move to the end of last year - this is a bad soln and needs updating
+  # data <- data %>%
+  #   arrange(game_id) %>%
+  #   group_by(team_home) %>%
+  #   mutate(team_position_home = ifelse(round_id == 1, lag(team_position_home), team_position_home)) %>%
+  #   group_by(team_away) %>%
+  #   mutate(team_position_away = ifelse(round_id == 1, lag(team_position_away), team_position_away)) %>%
+  #   ungroup() %>%
+  #   # fix intro for dolphins
+  #   mutate(team_position_home = ifelse(game_id == 20231110170, 17, team_position_home))
+  #   
     return(data)
   
 }
