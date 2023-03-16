29297e34f23ba4bc4145e7acf71d4449c6f727e4
levonrush <63217485+levonrush@users.noreply.github.com>
2023-03-16 15:43:03 +1100

    new features and benchmarking

--- R/make-predictions.R
+++ R/make-predictions.R
@@ -3,7 +3,11 @@ make_predicitons <- function(data, model, predictors, outcome_var = "home_team_r
   predictions <- data.frame(
     game_id = data$game_id,
     team_home = data$team_home,
+    position_home = data$position_home,
+    team_head_to_head_odds_home = data$team_head_to_head_odds_home,
     team_away = data$team_away,
+    position_away = data$position_away,
+    team_head_to_head_odds_away = data$team_head_to_head_odds_away,
     home_team_result = predict(model, data %>% select(all_of(c(predictors, outcome_var)))),
     home_team_win_prob = predict(model, data %>% select(all_of(c(predictors, outcome_var))), type = "prob")[,"Win"],
     home_team_draw_prob = predict(model, data %>% select(all_of(c(predictors, outcome_var))), type = "prob")[,"Draw"],
