fa690b0a11acdb467efa3c39a9ddfc735d70002c
Levon Rush <l.rush@nib.com.au>
2023-06-14 21:52:09 +1000

    test a few models - start

--- pipeline/model-training/modelling_functions.py
+++ pipeline/model-training/modelling_functions.py
@@ -26,10 +26,6 @@ def perform_rfe(estimator, data, k, opt_metric, maximise, steps=None, outcome_va
     X = one_hot_encode_features(data, predictors)
     y = data[outcome_var]
 
-    # One-hot encode categorical columns
-    object_columns = X.select_dtypes(include=['object']).columns
-    X = pd.get_dummies(X, columns=object_columns)
-
     # Scoring metric
     if opt_metric == 'ROC':
         scoring = make_scorer(roc_auc_score, multi_class="ovr", needs_proba=True)
@@ -37,30 +33,19 @@ def perform_rfe(estimator, data, k, opt_metric, maximise, steps=None, outcome_va
         scoring = 'accuracy'
 
     # Recursive Feature Elimination with cross-validation
-    rfecv = RFECV(estimator=estimator, step=1, n_jobs = -1, cv=StratifiedKFold(k), scoring=scoring)
+    rfecv = RFECV(estimator=estimator, step=1, n_jobs=-1, cv=StratifiedKFold(k), scoring=scoring)
     rfecv.fit(X, y)
-
-    # # Plotting number of features vs. cross-validation scores
-    # plt.figure()
-    # plt.xlabel("Number of features selected")
-    # plt.ylabel("Cross validation score (mean)")
-    # mean_scores = np.mean(rfecv.cv_scores_, axis=0)
-    # plt.plot(range(1, len(mean_scores) + 1), mean_scores)
-    # plt.show()
-
 
     # Optimal set of predictors (one-hot encoded)
     optimal_features = np.array(X.columns)[rfecv.support_]
 
-    return optimal_features, X.columns
+    # Return the one-hot encoded data and the optimal features
+    return X, y, optimal_features
 
 
-def train_tune_model(estimator, param_grid, data, predictors, outcome_var, num_folds=5, opt_metric='ROC', seed=69):
+
+def train_tune_model(estimator, param_grid, X, y, num_folds=5, opt_metric='ROC', seed=69):
     
-    # One-hot encode features
-    X = one_hot_encode_features(data, predictors)
-    y = data[outcome_var]
-
     # Scoring metric
     if opt_metric == 'ROC':
         scoring = make_scorer(roc_auc_score, multi_class="ovr", needs_proba=True)
@@ -68,7 +53,7 @@ def train_tune_model(estimator, param_grid, data, predictors, outcome_var, num_f
         scoring = 'accuracy'
 
     # Grid Search with cross-validation
-    cv = GridSearchCV(estimator, param_grid, cv=StratifiedKFold(num_folds), scoring=scoring, verbose=1, n_jobs = -1)
+    cv = GridSearchCV(estimator, param_grid, cv=StratifiedKFold(num_folds), scoring=scoring, verbose=1, n_jobs=-1)
     cv.fit(X, y)
     
     # Print results
@@ -77,25 +62,38 @@ def train_tune_model(estimator, param_grid, data, predictors, outcome_var, num_f
 
     return cv
 
-def training_pipeline(train_df, estimator, outcome_var, predictors):
+def training_pipeline(train_df, estimator, param_grid, outcome_var, predictors, opt_metric):
 
     label_encoder = LabelEncoder()
     train_df[outcome_var] = label_encoder.fit_transform(train_df[outcome_var])
 
-    if estimator == 'rf':
-        rf_estimator = RandomForestClassifier(n_jobs = -1)
-        rf_param_grid = {'n_estimators': [10, 50, 100], 'max_features': ['auto', 'sqrt', 'log2']}
-        # Capture the returned values
-        opt_rf_predictors, all_rf_features = perform_rfe(rf_estimator, data=train_df, k=5, opt_metric='ROC', maximise=True, outcome_var=outcome_var, predictors=predictors)
-        # Use opt_rf_predictors as the predictors parameter
-        cv = train_tune_model(rf_estimator, rf_param_grid, data=train_df, predictors=opt_rf_predictors, outcome_var=outcome_var, opt_metric='ROC')
+    # Capture the returned values
+    X_encoded, y_encoded, optimal_features = perform_rfe(estimator, data=train_df, k=5, opt_metric=opt_metric, maximise=True, outcome_var=outcome_var, predictors=predictors)
     
-    elif estimator == 'gbm':
-        gb_estimator = GradientBoostingClassifier(n_jobs = -1)
-        gb_param_grid = {'n_estimators': [50, 100, 200], 'learning_rate': [0.01, 0.1, 0.2]}
-        # Capture the returned values
-        opt_gb_predictors, all_gb_features = perform_rfe(gb_estimator, data=train_df, k=5, opt_metric='ROC', maximise=True, outcome_var=outcome_var, predictors=predictors)
-        # Use opt_gb_predictors as the predictors parameter
-        cv = train_tune_model(gb_estimator, gb_param_grid, data=train_df, predictors=opt_gb_predictors, outcome_var=outcome_var, opt_metric='ROC')
+    # Train and tune the model using the one-hot encoded data
+    cv = train_tune_model(estimator, param_grid, X_encoded[optimal_features], y_encoded)
 
     return cv
+
+# def training_pipeline(train_df, estimator, outcome_var, predictors, opt_metric):
+
+    # label_encoder = LabelEncoder()
+    # train_df[outcome_var] = label_encoder.fit_transform(train_df[outcome_var])
+
+    # if estimator == 'rf':
+    #     rf_estimator = RandomForestClassifier(n_jobs = -1)
+    #     rf_param_grid = {'n_estimators': [10, 50, 100], 'max_features': ['auto', 'sqrt', 'log2']}
+    #     # Capture the returned values
+    #     X_encoded, y_encoded, optimal_features = perform_rfe(rf_estimator, data=train_df, k=5, opt_metric=opt_metric, maximise=True, outcome_var=outcome_var, predictors=predictors)
+    #     # Train and tune the model using the one-hot encoded data
+    #     cv = train_tune_model(rf_estimator, rf_param_grid, X_encoded[optimal_features], y_encoded)
+    
+    # elif estimator == 'gbm':
+    #     gb_estimator = GradientBoostingClassifier()
+    #     gb_param_grid = {'n_estimators': [50, 100, 200], 'learning_rate': [0.01, 0.1, 0.2]}
+    #     # Capture the returned values
+    #     X_encoded, y_encoded, optimal_features = perform_rfe(gb_estimator, data=train_df, k=5, opt_metric=opt_metric, maximise=True, outcome_var=outcome_var, predictors=predictors)
+    #     # Use opt_gb_predictors as the predictors parameter
+    #     cv = train_tune_model(gb_estimator, gb_param_grid, X_encoded[optimal_features], y_encoded)
+
+    # return cv
