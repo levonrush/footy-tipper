f1537995887de4d1582d63e8564430af2f26ddc6
Levon Rush <l.rush@nib.com.au>
2023-06-14 17:44:51 +1000

    reconstructing pipeline
    - finished data prep
    - new sqlite db
    - started on modelling in python

--- pipeline/data-prep/data-pipeline.R
+++ pipeline/data-prep/data-pipeline.R
@@ -1,9 +1,9 @@
 data_pipeline <- function(year_span, pipeline, form_period, carry_over, k_val, elo_init) {
-
+  
     footy_tipping_data <- get_data(year_span = year_span) %>%
       clean_data() %>%
       fixture_result(pipeline = pipeline) %>%
-      feature_engineering(form_period = form_period, pipeline = pipeline) %>%
+      feature_engineering(form_period = form_period) %>%
       elo_variables(carry_over = carry_over, k_val = k_val, elo_init = elo_init) %>%
       home_ground_advantage() %>%
       filter(competition_year != min(competition_year),
