bb5ac321b0f83c4a43636d67aa449ff8f80765b5
Levon Rush <l.rush@nib.com.au>
2023-06-24 15:53:58 +1000

    fix auth 2

--- footy-tipper.R
+++ footy-tipper.R
@@ -1,8 +1,5 @@
 ### The Footy Tipper Pipeline ###
 
-# first, decide what type of run we are doing
-prod_run = T
-
 # define where the pipeline lives
 library(here)
 i_am("footy-tipper.R")
@@ -24,8 +21,8 @@ detect_set_environment(prod_run = prod_run)
 # define the paths to the notebooks
 data_prep_rmd <- paste0(here(), "/pipeline/data-prep/data-prep.Rmd")
 model_training_ipynb <- paste0(here(), "/pipeline/model-training/model-training.ipynb")
-# use_predictions_rmd <- paste0(here(), "/pipeline/use-predictions/use-predictions.Rmd")
-use_predictions_r <- paste0(here(), "/pipeline/use-predictions/use-predictions.R")
+use_predictions_rmd <- paste0(here(), "/pipeline/use-predictions/use-predictions.Rmd")
+#use_predictions_r <- paste0(here(), "/pipeline/use-predictions/use-predictions.R")
 
 # Execute the data-prep.Rmd notebook
 rmarkdown::render(
@@ -37,8 +34,8 @@ rmarkdown::render(
 system(paste("jupyter nbconvert --to notebook --execute", model_training_ipynb))
 
 # Execute the use-predictions.Rmd notebook
-# rmarkdown::render(
-#     use_predictions_rmd,
-#     output_format = "github_document"
-# )
-source(use_predictions_r)
+rmarkdown::render(
+    use_predictions_rmd,
+    output_format = "github_document"
+)
+# source(use_predictions_r)
