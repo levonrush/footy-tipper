6d2aa9329369981b079784985364a1e838b2974a
Levon Rush <l.rush@nib.com.au>
2023-07-05 18:13:51 +1000

    comment data functions

--- pipeline/data-prep/data-pipeline.R
+++ pipeline/data-prep/data-pipeline.R
@@ -1,5 +1,14 @@
 data_pipeline <- function(year_span, pipeline, form_period, carry_over, k_val, elo_init) {
 
+    # The 'data_pipeline' function acts as a wrapper for running the entire data processing pipeline.
+    
+    # Step 1: 'get_data' function is called to fetch data for the specified range of years.
+    # Step 2: 'clean_data' function is called for data cleaning.
+    # Step 3: 'fixture_result' function is called to get the fixture and result data.
+    # Step 4: 'feature_engineering' function is called for feature extraction and engineering.
+    # Step 5: 'elo_variables' function is called for ELO rating calculations.
+    # Step 6: 'home_ground_advantage' function is called to calculate the home ground advantage.
+    
     footy_tipping_data <- get_data(year_span = year_span) %>%
       clean_data() %>%
       fixture_result(pipeline = pipeline) %>%
@@ -10,6 +19,8 @@ data_pipeline <- function(year_span, pipeline, form_period, carry_over, k_val, e
       ) %>%
       home_ground_advantage()
 
+    # The final processed data is returned.
     return(footy_tipping_data)
 
 }
+
