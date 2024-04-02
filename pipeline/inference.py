09dd2c4570820ad8c7588addf30f5ca9ef16f87e
Levon Rush <l.rush@nib.com.au>
2024-04-02 18:06:13 +1100

    Update inference.py

--- pipeline/inference.py
+++ pipeline/inference.py
@@ -1,23 +1,23 @@
+# Description: This script is used to make predictions using the model and save the predictions to the database.
+
 # import libraries
 import os
 import sys
 import pathlib
-from dotenv import load_dotenv
 
-# this takes us down a level to the root of the project
-sys.path.insert(0, os.path.abspath('..'))
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# get the parent directory
+parent_dir = os.path.dirname(script_dir)
+
+# add the parent directory to the system path
+sys.path.insert(0, parent_dir)
 
 # import functions from common like this:
-from pipeline.common.model_training import (
-    modelling_functions as mf,
-    model_properties as mp,
-    training_config as tc,
-    prediction_functions as pf,
-)
-from pipeline.common.use_predictions import sending_functions as sf
+from pipeline.common.model_prediciton import prediction_functions as pf
 
 # Get to the root directory
-project_root = pathlib.Path().absolute().parent
+project_root = pathlib.Path().absolute()
 
 # Now construct the relative path to your SQLite database
 db_path = project_root / "data" / "footy-tipper-db.sqlite"
@@ -38,6 +38,3 @@ pf.save_predictions_to_db(
     project_root / 'pipeline/common/sql/create_table.sql', 
     project_root / 'pipeline/common/sql/insert_into_table.sql'
     )
-
-# send predictions to users
-
