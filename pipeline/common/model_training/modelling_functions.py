ccb0c65c1da50e13e26d716f958ddf9d1d0fa558
Levon Rush <l.rush@nib.com.au>
2024-07-16 14:17:47 +1000

    update with scaled up modelling params

--- pipeline/common/model_training/modelling_functions.py
+++ pipeline/common/model_training/modelling_functions.py
@@ -71,7 +71,7 @@ def create_pipeline(estimator, param_grid, use_rfe, num_folds, opt_metric, cat_c
         cv=num_folds, 
         scoring=opt_metric, 
         n_jobs=-1,
-        population_size=1000,
+        population_size=100,
         generations=25,
         crossover_probability=0.5, 
         mutation_probability=0.2, 
